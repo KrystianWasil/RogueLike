9a4e93a4ad6736d8a35f8bcbe4ecb884c09cc0a2
Krystian Wasil <wasilkrystian78@gmail.com>
2023-11-09 15:45:21 +0000

    Upload New File

---
+++ Items.h
@@ -0,0 +1,14 @@
+//
+// Created by wasil on 09.11.2023.
+//
+
+#ifndef GAMEROGUELIKE_ITEMS_H
+#define GAMEROGUELIKE_ITEMS_H
+
+
+class Items {
+public:
+    char health = '&';
+};
+
+#endif //GAMEROGUELIKE_ITEMS_H
