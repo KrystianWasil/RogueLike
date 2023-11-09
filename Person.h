bfb2f143a69ca44973079d4b9cc3ecc81f52df81
Krystian Wasil <wasilkrystian78@gmail.com>
2023-11-09 15:46:11 +0000

    Upload New File

---
+++ Person.h
@@ -0,0 +1,21 @@
+//
+// Created by wasil on 09.11.2023.
+//
+
+#ifndef GAMEROGUELIKE_PERSON_H
+#define GAMEROGUELIKE_PERSON_H
+
+
+class Person {
+    public:
+        int x = 1;
+        int y = 1;
+        char character = '@';
+        int max_health = 100;
+        int current_health = 20;
+
+
+};
+
+
+#endif //GAMEROGUELIKE_PERSON_H
