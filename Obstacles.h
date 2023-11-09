21719825b84054818fc91b45d5027a7827bc2d28
Krystian Wasil <wasilkrystian78@gmail.com>
2023-11-09 15:45:51 +0000

    Upload New File

---
+++ Obstacles.h
@@ -0,0 +1,15 @@
+//
+// Created by wasil on 09.11.2023.
+//
+
+#ifndef GAMEROGUELIKE_OBSTACLES_H
+#define GAMEROGUELIKE_OBSTACLES_H
+
+
+class Obstacles {
+public:
+    char bush = '#';
+};
+
+
+#endif //GAMEROGUELIKE_OBSTACLES_H
