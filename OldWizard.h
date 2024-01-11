90d3f60db3aa42be8ad8f885a527153081ffb802
Krystian Wasil <wasilkrystian78@gmail.com>
2024-01-11 16:01:56 +0000

    Add new file

---
+++ OldWizard.h
@@ -0,0 +1,17 @@
+//
+// Created by wasil on 12.12.2023.
+//
+
+#ifndef ROGUELIKE_OLDWIZARD_H
+#define ROGUELIKE_OLDWIZARD_H
+
+
+#include "Entity.h"
+
+class OldWizard : public Entity{
+public:
+    OldWizard() : Entity('W',"wizzo",100,50,12,100,0,1,40,1,{},{}) {}
+};
+
+
+#endif //ROGUELIKE_OLDWIZARD_H
