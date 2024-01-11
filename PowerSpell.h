b74004554613b70a3b9d36dfddd0f0c576821906
Krystian Wasil <wasilkrystian78@gmail.com>
2024-01-11 16:02:22 +0000

    Add new file

---
+++ PowerSpell.h
@@ -0,0 +1,16 @@
+//
+// Created by wasil on 21.12.2023.
+//
+
+#ifndef ROGUELIKE_POWERSPELL_H
+#define ROGUELIKE_POWERSPELL_H
+
+
+#include "Item.h"
+
+class PowerSpell : public Item{
+    PowerSpell() : Item('o', 0, 0, 0, 0, 8,"PowerSpell") {}
+};
+
+
+#endif //ROGUELIKE_POWERSPELL_H
