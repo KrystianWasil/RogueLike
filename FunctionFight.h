b2e0a4fa12feb36120146ff54e43e358a34211ca
Krystian Wasil <wasilkrystian78@gmail.com>
2024-01-11 15:56:00 +0000

    Add new file

---
+++ FunctionFight.h
@@ -0,0 +1,12 @@
+//
+// Created by wasil on 24.11.2023.
+//
+
+#ifndef ROGUELIKE_FUNCTIONFIGHT_H
+#define ROGUELIKE_FUNCTIONFIGHT_H
+
+
+bool DidYouHit(int, int, int);
+
+
+#endif //ROGUELIKE_FUNCTIONFIGHT_H
