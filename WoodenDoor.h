2ed4ac04388ffb88ec4fef011d9fe69c0a5c3853
Krystian Wasil <wasilkrystian78@gmail.com>
2024-01-11 16:06:34 +0000

    Add new file

---
+++ WoodenDoor.h
@@ -0,0 +1,17 @@
+//
+// Created by wasil on 10.11.2023.
+//
+
+#ifndef GAMEROGUELIKE_WOODENDOOR_H
+#define GAMEROGUELIKE_WOODENDOOR_H
+
+
+#include "Obstacles.h"
+
+class WoodenDoor : public Obstacles {
+public:
+    WoodenDoor() : Obstacles('/') {}
+};
+
+
+#endif //GAMEROGUELIKE_WOODENDOOR_H
