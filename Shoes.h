3eacc6f13a8dcdebf28b08ba6fdb5127d9b2ca44
Krystian Wasil <wasilkrystian78@gmail.com>
2024-01-11 16:03:34 +0000

    Add new file

---
+++ Shoes.h
@@ -0,0 +1,19 @@
+//
+// Created by wasil on 21.12.2023.
+//
+
+#ifndef ROGUELIKE_SHOES_H
+#define ROGUELIKE_SHOES_H
+
+
+#include "Item.h"
+
+class Shoes : public Item {
+public:
+    Shoes() : Item('l',0,0,0,5,0,"Shoes") {}
+
+
+};
+
+
+#endif //ROGUELIKE_SHOES_H
