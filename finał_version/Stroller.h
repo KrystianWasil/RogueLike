d7f485dc7837dc5bfcb6c9fd13352f003d38e63d
Krystian Wasil <wasilkrystian78@gmail.com>
2024-02-04 00:26:48 +0000

    Upload New File

---
+++ finał_version/Stroller.h
@@ -0,0 +1,17 @@
+//
+// Created by Krystian Wasil on 25/01/2024.
+//
+
+#ifndef ROGUELIKE_STROLLER_H
+#define ROGUELIKE_STROLLER_H
+
+
+#include "Tile.h"
+
+class Stroller : public Tile{
+public:
+    Stroller() : Tile('_') {}
+};
+
+
+#endif //ROGUELIKE_STROLLER_H
